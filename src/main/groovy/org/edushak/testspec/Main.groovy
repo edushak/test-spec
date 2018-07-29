42200009ac85c445243684c22fb72ce484479a87
edushak <edushak@yahoo.com>
2018-07-29 14:25:22 -0400

    Added to ability to execute code and external commands

---
+++ src/main/groovy/org/edushak/testspec/Main.groovy
@@ -0,0 +1,28 @@
+package org.edushak.testspec
+
+import cucumber.runtime.ClassFinder
+import cucumber.runtime.Runtime
+import cucumber.runtime.RuntimeOptions
+import cucumber.runtime.io.MultiLoader
+import cucumber.runtime.io.ResourceLoader
+import cucumber.runtime.io.ResourceLoaderClassFinder
+
+class Main {
+    public Main() {
+    }
+
+    public static void main(String[] argv) throws Throwable {
+        byte exitstatus = run(argv, Thread.currentThread().getContextClassLoader());
+        System.exit(exitstatus);
+    }
+
+    public static byte run(String[] argv, ClassLoader classLoader) throws IOException {
+        RuntimeOptions runtimeOptions = new RuntimeOptions(new ArrayList(Arrays.asList(argv)));
+        ResourceLoader resourceLoader = new MultiLoader(classLoader);
+        ClassFinder classFinder = new ResourceLoaderClassFinder(resourceLoader, classLoader);
+        Runtime runtime = new Runtime(resourceLoader, classFinder, classLoader, runtimeOptions);
+        runtime.run();
+        return runtime.exitStatus();
+    }
+
+}
--- src/main/groovy/org/edushak/testspec/TestSpecWorld.groovy
+++ src/main/groovy/org/edushak/testspec/TestSpecWorld.groovy
@@ -1,17 +1,53 @@
 package org.edushak.testspec
 
+import groovy.util.logging.Slf4j
+import groovyx.gpars.GParsPool
+import groovyx.gpars.dataflow.Promise
+import jsr166y.ForkJoinPool
+import org.apache.commons.lang.StringUtils
+import org.codehaus.groovy.jsr223.GroovyScriptEngineFactory
+import org.codehaus.groovy.tools.shell.util.NoExitSecurityManager
 import org.edushak.testspec.util.Helper
 import org.edushak.testspec.util.RestHelper
 
+import javax.script.ScriptEngine
+import java.util.concurrent.ConcurrentLinkedDeque
+
+@Slf4j
 class TestSpecWorld {
     static TestSpecWorld currentWorld
+    static Queue<Promise> _promises = new ConcurrentLinkedDeque<Promise>()
 
     Binding binding
+    SecurityManager defaultManager = System.securityManager,
+                    noExitManager = new NoExitSecurityManager()
+    Closure executeCodeAsync
+    ForkJoinPool pool = new ForkJoinPool()
+
+    TestSpecWorld() {
+        GParsPool.withExistingPool(pool) {
+            executeCodeAsync = { code, binding ->
+                try {
+                    executeCode(code, binding)
+                } catch(Throwable th) {
+                    log.error("Failed to run executeCode() asynchronously", th)
+                }
+            }.asyncFun()
+        }
+    }
+
     void setBinding(Binding binding) {
         this.binding = binding
         if (!binding.hasVariable('_restCall')) {
             binding.setVariable('_restCall', RestHelper.&call)
         }
+    }
+
+    boolean isVar(String str) {
+        isVar(str, binding)
+    }
+    static boolean isVar(String str, Binding binding) {
+        str && str != '$' && binding.hasVariable(str)
     }
 
     static noQuotes(Object str) {
@@ -25,6 +61,77 @@ class TestSpecWorld {
         normalizeValue(value,binding)
     }
 
+    def normalizeParameter(String input, boolean preserveQuotes = false) {
+        normalizeParameter(input, preserveQuotes, binding)
+    }
+    static normalizeParameter(String input, boolean preserveQuotes, Binding binding) {
+        if (!preserveQuotes) {
+            input = noQuotes(input)
+        }
+        def inputValue
+        String actualParameters = ''
+        boolean isParametrizedIdentifier = isParametrizedIdentifier(input)
+        if (isParametrizedIdentifier) {
+            List tokens = input.tokenize('(')
+            if (isVar(tokens.first(), binding)) {
+                inputValue = var(tokens.first(), binding)
+            } else {
+                inputValue = input
+                isParametrizedIdentifier = false
+            }
+            if (tokens.size() == 2) {
+                actualParameters = tokens.last()[0..-2]
+            } else {
+                throw new Exception("Bad formatted identifier call: $input;" +
+                        "It should look like: identifier('parameter')")
+            }
+        } else {
+            if (isVar(input, binding)) {
+                inputValue = var(input, binding)
+            } else {
+                inputValue = input
+                if (Helper.isSelector(input)) {
+
+                }
+            }
+        }
+
+        if (inputValue instanceof CharSequence) {
+            boolean isParametrizedValue = inputValue instanceof CharSequence ? isParametrizedValue(inputValue) : false
+            if (isParametrizedIdentifier && !isParametrizedValue) {
+                throw new Exception("identifier IS parametrized, while value IS NOT")
+            }
+            if (!isParametrizedIdentifier && isParametrizedValue) {
+                throw new Exception("identifier is NOT parametrized, while value IS")
+            }
+
+            if (inputValue.contains('$(By.')) {
+                inputValue = inputValue.replace('$(By.', '$(org.openqa.selenium.By.')
+            } else if (inputValue.contains('$x(')) {
+                inputValue = StringUtils.replace(inputValue.toString(), '$x(', '$(org.openqa.selenium.By.xpath(')
+                int lastClosingParentesis = inputValue.lastIndexOf(')')
+                inputValue = inputValue.substring(0,lastClosingParentesis) + ')' + inputValue.substring(lastClosingParentesis)
+            }
+        }
+
+        if (isParametrizedIdentifier || Helper.isClosure(inputValue)) {
+            inputValue = inputValue.trim()
+            inputValue += ".call($actualParameters);"
+        }
+
+        return inputValue
+    }
+
+    static boolean isParametrizedIdentifier(String input) {
+        if (!input) {return null}
+        !Helper.isSelector(input) && input.indexOf(')') > input.indexOf('(') &&
+                input.substring(input.length()-1) == ')'
+
+    }
+    static boolean isParametrizedValue(String input) {
+        Helper.isParametrizedClosure(input)
+    }
+
     static String normalizeValue(String value, Binding binding) {
         var(noQuotes(value) as String, binding)
     }
@@ -35,4 +142,60 @@ class TestSpecWorld {
     static Object var(String str, Binding binding) {
         binding.hasVariable(str) ? binding.getVariable(str) : str
     }
+
+
+    Closure getEngine = { String lang, Binding ->
+        if (!binding.hasVariable('__engines')) {
+            binding.setVariable('__engines', [:])
+        }
+        if (binding.__engines[lang] == null) {
+            if (lang == 'groovy') {
+                binding.__engines[lang] = new GroovyScriptEngineFactory().getScriptEngine()
+            }
+            // TODO: add more lang support?
+        }
+        binding.__engines[lang]
+    }
+
+    Closure executeCodeFacade = { String async, String code ->
+        Binding _binding = new Binding(binding.variables)
+        if (async) {
+            GParsPool.withExistingPool(pool) {
+                Promise p = executeCodeAsync(code, _binding)
+                _promises.offer(p)
+            }
+        } else {
+            executeCode(code, _binding)
+        }
+    }
+
+    Closure executeCode = { String code, Binding binding ->
+        if (code.contains('exit')) {
+            System.setSecurityManager(noExitManager)
+        }
+        ScriptEngine engine = getEngine('groovy', binding)
+        if (!engine) {
+            throw new Exception("No support for chosen language")
+        }
+        Object retValue = null
+        try {
+            code = Helper.noQuotes(code)
+            if (Helper.isClosure(code) && !code.contains('.call()') && !code.trim().endsWith('()')){
+                code = code + '.call()'
+            }
+            retValue = engine.eval(code, binding)
+        } catch (javax.script.ScriptException se) {
+            if (se.cause?.cause instanceof  MissingPropertyException) {
+                throw new Exception("You might be accessing a variable that has not been defined yet!", se.cause.cause)
+            } else {
+                throw se
+            }
+        } finally {
+            if (code.contains('exit')) {
+                System.setSecurityManager(defaultManager)
+            }
+        }
+        retValue
+    }
+
 }
